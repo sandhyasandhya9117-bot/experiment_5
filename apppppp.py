8f87c6fb6b415f345b832b8fddb7952d25ce2b0b
sandhya <sandhya@gmail.com>
2025-08-25 11:40:36 +0530

    this exp5 chages

---
+++ apppppp.py
@@ -0,0 +1,9 @@
+def main():
+    print("Hello, Jenkins! This is a Python build test.")
+    # simple calculation
+    a = 10
+    b = 5
+    print(f"Sum of {a} and {b} is {a + b}")
+
+if _name_ == "_main_":
+    main()
